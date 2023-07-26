e298de9110aaf92abf0c9bd889e3a8af0b8b5dd8
Rysth <johnpalacios.t@gmail.com>
2023-07-26 13:21:31 -0500

    Fix semantic errors.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import { createGame, getScores, setScore } from './modules/game';
 import './styles.css';
 
-const ID = await createGame('My Awesome Game');
+const ID = await createGame('RysthCraft');
 const btnRefresh = document.querySelector('#btn-refresh');
 const btnSubmit = document.querySelector('#btn-submit');
 const form = document.querySelector('#form');
 
 btnRefresh.addEventListener('click', async () => {
   const { result } = await getScores(ID);
-  result.forEach((element) =>
-    console.log(`User: ${element.user}, Score: ${element.score}`)
-  );
+  if (result.length >= 1) {
+    const userList = document.querySelector('#user-list');
+
+    userList.innerHTML = result
+      .map((element, index) => {
+        const value = index + 1;
+        const listItemClass =
+          value % 2 === 0 && index > 0 ? 'list-item list-item-accent' : 'list-item';
+        return `<li class="${listItemClass}">${element.user}: ${element.score}</li>`;
+      })
+      .join('');
+  }
 });
 
 btnSubmit.addEventListener('click', async () => {
